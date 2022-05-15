dd963d2ec912b45a78407017638aa4dd4ee0caa8
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-15 15:50:47 +0300

    arrays and objects types added

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const number1 = document.getElementById("num1");
 const number2 = document.getElementById("num2");
 const button = document.querySelector("button"); // Tell typescript that wont be null
+const numberArray = [];
+const stringArray = [];
 const add = (number1, number2) => {
-    return number1 + number2;
+    if (typeof number1 === "number" && typeof number2 === "number") {
+        return number1 + number2;
+    }
+    if (typeof number1 === "string" && typeof number2 === "string") {
+        return number1 + " " + number2;
+    }
+    return +number1 + +number2;
 };
-console.log(add(1, 5));
+function printout(resultObj) {
+    console.log([resultObj.value, resultObj.timestamp]);
+}
 // if (button) {
 //   button.addEventListener("click", () => {
 //     const num1 = number1.value;
@@ -17,6 +27,11 @@ console.log(add(1, 5));
 button.addEventListener("click", () => {
     const num1 = number1.value;
     const num2 = number2.value;
-    const result = add(+num1, +num2);
-    console.log(result);
+    const numberResult = add(+num1, +num2);
+    numberArray.push(numberResult);
+    const stringResult = add(num1, num2);
+    stringArray.push(stringResult);
+    printout({ value: numberResult, timestamp: new Date() });
+    console.log(numberArray);
+    console.log(stringArray);
 });
--- app.ts
+++ app.ts
@@ -1,12 +1,21 @@
 const number1 = document.getElementById("num1") as HTMLInputElement;
 const number2 = document.getElementById("num2") as HTMLInputElement;
 const button = document.querySelector("button")!; // Tell typescript that wont be null
-
-const add = (number1: number, number2: number) => {
-  return number1 + number2;
+const numberArray: number[] = [];
+const stringArray: string[] = [];
+const add = (number1: number | string, number2: number | String) => {
+  if (typeof number1 === "number" && typeof number2 === "number") {
+    return number1 + number2;
+  }
+  if (typeof number1 === "string" && typeof number2 === "string") {
+    return number1 + " " + number2;
+  }
+  return +number1 + +number2;
 };
 
-console.log(add(1, 5));
+function printout(resultObj: { value: number; timestamp: Date }) {
+  console.log([resultObj.value, resultObj.timestamp]);
+}
 
 // if (button) {
 //   button.addEventListener("click", () => {
@@ -20,6 +29,11 @@ console.log(add(1, 5));
 button.addEventListener("click", () => {
   const num1 = number1.value;
   const num2 = number2.value;
-  const result = add(+num1, +num2);
-  console.log(result);
+  const numberResult = add(+num1, +num2) as number;
+  numberArray.push(numberResult);
+  const stringResult = add(num1, num2) as string;
+  stringArray.push(stringResult);
+  printout({ value: numberResult, timestamp: new Date() });
+  console.log(numberArray);
+  console.log(stringArray);
 });
